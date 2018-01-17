959e965df8475ad9a39a32007e3fc964e7f7fbd2
neboskrebnut <ivanov.arseniy@gmail.com>
2018-01-17 18:55:29 -0500

    constructor for customer 1/2

--- Problem1/Customer.cpp
+++ Problem1/Customer.cpp
@@ -1,11 +1,25 @@
 #include "Customer.h"
+#include <iostream>
+#include <string>
 
-
-
+using namespace std;
 Customer::Customer()
 {
+	string dateOfBirth;
 
-}
+	cout << "\nEnter new customer's name: ";
+	cin >> Name;
+	cout << "\nEnter customer's address:  ";
+	cin >> Address;
+	cout << "\nEnter customer's phone number: ";
+	cin >> Tel;
+	cout << "\nEnter customer's date of birth (format: DD/MM/YYYY): ";
+	cin >> dateOfBirth;
+
+	//Convert string to Date obj.
+
+
+	
 
 
 Customer::~Customer()
